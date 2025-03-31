b15937ba95cdd73249cd9cd4b067a09133b7e1a6
Madindig <luis.andrew1023@gmail.com>
2025-03-31 17:47:50 +0800

    Added serach feature

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -6,7 +6,33 @@ const Establishment = require("../models/Establishment");
 router.get("/", async (req, res) => {
     try {
         const establishments = await Establishment.find().lean();
-        res.render("home", { establishments, user: req.session.user || null });
+        res.render("home", { establishments, user: req.session.user || null, searchQuery : "", amenities : [], locations : [], ratings : [] });
+    } catch (error) {
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+// Search results
+router.get("/results", async (req, res) => {
+    const searchString = req.query.search || '';
+    // Hope you like ternary operators
+    // Ensures that selectedAmenities and selectedLocations is always an array
+    const selectedAmenities = Array.isArray(req.query.amenities) ? req.query.amenities : req.query.amenities ? [req.query.amenities] : [];
+    const selectedLocations = Array.isArray(req.query.regions) ? req.query.regions : req.query.regions ? [req.query.regions] : [];
+    const selectedRatings = Array.isArray(req.query.ratings) ? req.query.ratings.map(Number) : req.query.ratings ? [Number(req.query.ratings)] : [];
+
+    try {
+        const searchFilter = {
+            // Used ...(condition && filter) so that filter that
+            ...(searchString != '' && { name : { $regex : searchString, $options : 'i' } }), // case insensitive substrings
+            ...(selectedAmenities.length > 0 && { amenities: { $all: selectedAmenities } }), // Use $all for AND instead of OR
+            ...(selectedLocations.length > 0 && { location: { $in: selectedLocations } }),
+            ...(selectedRatings.length > 0 && { $expr: { $in: [{ $round: ["$rating"] }, selectedRatings]} }) // Round the rating first to nearest integer then check if its in the ratings array
+        };
+
+        const establishments = await Establishment.find(searchFilter).lean();
+        
+        res.render("home", { establishments, user: req.session.user || null, searchQuery : searchString, amenities : selectedAmenities, locations : selectedLocations, ratings : selectedRatings });
     } catch (error) {
         res.status(500).send("Internal Server Error");
     }
